4e45ef1fc129ee49f058bb7b6b2b1996f4d31b07
Dieter Rosch <dieter.rosch@gmail.com>
2022-10-28 16:05:53 +0200

    m

--- generate_jportal.sh
+++ generate_jportal.sh
@@ -1,11 +1,11 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-echo $SCRIPT_DIR
+#echo $SCRIPT_DIR
 
 #only needed to pull latest version
 docker pull ghcr.io/si-gen/jportal2:latest
 
-docker run --rm -v ${SCRIPT_DIR}:/local:Z ghcr.io/si-gen/jportal2:latest \
+docker run --rm -v ${SCRIPT_DIR}:/local ghcr.io/si-gen/jportal2:latest \
                       --inputdir=/local/sql/si \
                       --builtin-generator PostgresDDL:/local/generated_sources/generated_sql \
                       --template-generator SQLAlchemy:/local/generated_sources/python/jportal \
-                      --download-template "SQLAlchemy:https://github.com/SI-Gen/jportal2-generator-vanguard-sqlalchemy/archive/refs/tags/2.0.zip|stripBaseDir"                      
+                      --download-template "SQLAlchemy:https://github.com/SI-Gen/jportal2-generator-vanguard-sqlalchemy/archive/refs/tags/1.8.zip|stripBaseDir"                      
