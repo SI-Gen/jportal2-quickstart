b740639caaf5036dfa7335ac5bcbb445a83139c8
dieterrosch <dieterrosch@users.noreply.github.com>
2023-03-11 10:51:14 +0200

    Update generate_jportal.sh

--- generate_jportal.sh
+++ generate_jportal.sh
@@ -11,5 +11,5 @@ docker run --rm -v ${SCRIPT_DIR}:/local ghcr.io/si-gen/jportal2:latest \
                       --builtin-generator PostgresDDL:/local/generated_sources/generated_sql \
                       --builtin-generator JavaJCCode:/local/generated_sources/java \
                       --template-generator SQLAlchemy:/local/generated_sources/python/jportal \
-                      --download-template "SQLAlchemy:https://github.com/SI-Gen/jportal2-generator-vanguard-sqlalchemy/archive/refs/tags/1.8.zip|stripBaseDir"               
+                      --download-template "SQLAlchemy:https://github.com/SI-Gen/jportal2-generator-vanguard-sqlalchemy/archive/refs/tags/2.4.zip|stripBaseDir"               
                        
